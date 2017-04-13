8f22b13e633ac328f4767d79858a5cea7ec7776f
Andrei <anznobishchev@gmail.com>
2017-04-13 23:37:19 +0400

    Add files via upload
    mass selection

--- Scripts/Arguments/Energy_Time_Spectrogram_Args.m
+++ Scripts/Arguments/Energy_Time_Spectrogram_Args.m
@@ -1,6 +1,10 @@
 function [Args] = Energy_Time_Spectrogram_Args()
 
-    field1 = 'file';
-    value1 = {{'c6_32e64m', ''}}; %{filetype, path to the file}
+    field1 = 'Mass';
+    value1 = {{'Cumulative', 0}; {'H+', 1}; {'He+', 4}; {'C+', 12}; {'O+', 16}; {'O2+', 32}; {'CO2+', 44}};
+    field2 = 'LogScale';
+    value2 = {{'on', 1}; {'off', 0}};
+    field3 = 'file';
+    value3 = {{'c6_32e64m', ''}}; %{filetype, path to the file}
     
-    Args = struct(field1, {value1});
+    Args = struct(field1, {value1}, field2, {value2}, field3, {value3});
