c1bc95426aa516d7fa421c0edb524efd0f198c45
Andrei <anznobishchev@gmail.com>
2018-02-02 19:26:57 +0300

    Add files via upload

--- Scripts/NumberDensity_Time.m
+++ Scripts/NumberDensity_Time.m
@@ -1,94 +1,133 @@
 function [message, error] = NumberDensity_Time (ax, filefolderpath, date, start_time, stop_time, specific_args)
+    
+    mass = specific_args{1, 1};
+    log = specific_args{1, 2};
+    file_type = specific_args{1, 3};
+    
+    if strcmp(file_type, 'd1')
 
-    [filename, message, error] = get_file(date, filefolderpath);
-    if ~isempty(filename)
-        
-        mass = specific_args{1, 1}; 
-        log = specific_args{1, 2};
+        [filename, message, error] = get_file(date, filefolderpath);
+        if ~isempty(filename)
 
-        epoch = spdfcdfread(filename, 'variables', 'epoch');
-        eflux = spdfcdfread(filename, 'variables', 'eflux');
-        energy = spdfcdfread(filename, 'variables', 'energy');
-        denergy = spdfcdfread(filename, 'variables', 'denergy');
-        domega = spdfcdfread(filename, 'variables', 'domega');
-        swp_ind = spdfcdfread(filename, 'variables', 'swp_ind');
-        mass_arr = spdfcdfread(filename, 'variables', 'mass_arr');
-        nenergy = spdfcdfread(filename, 'variables', 'nenergy');
-        nanode = spdfcdfread(filename, 'variables', 'nanode');
-        ndef = spdfcdfread(filename, 'variables', 'ndef');
+            epoch = spdfcdfread(filename, 'variables', 'epoch');
+            eflux = spdfcdfread(filename, 'variables', 'eflux');
+            energy = spdfcdfread(filename, 'variables', 'energy');
+            denergy = spdfcdfread(filename, 'variables', 'denergy');
+            domega = spdfcdfread(filename, 'variables', 'domega');
+            swp_ind = spdfcdfread(filename, 'variables', 'swp_ind');
+            mass_arr = spdfcdfread(filename, 'variables', 'mass_arr');
+            nenergy = spdfcdfread(filename, 'variables', 'nenergy');
+            nanode = spdfcdfread(filename, 'variables', 'nanode');
+            ndef = spdfcdfread(filename, 'variables', 'ndef');
 
-        start_str = start_time; stop_str = stop_time; %save str version of start, stop times
-        %time
-        difference = abs(datenum(datestr(epoch, 'HH:MM:SS')) - datenum(start_time));
-        start_time = find (difference == min(difference), 1);
-        difference = abs(datenum(datestr(epoch, 'HH:MM:SS')) - datenum(stop_time));
-        stop_time = find (difference == min(difference), 1);
+            start_str = start_time; stop_str = stop_time; %save str version of start, stop times
+            %time
+            difference = abs(datenum(datestr(epoch, 'HH:MM:SS')) - datenum(start_time));
+            start_time = find (difference == min(difference), 1);
+            difference = abs(datenum(datestr(epoch, 'HH:MM:SS')) - datenum(stop_time));
+            stop_time = find (difference == min(difference), 1);
 
-        timefrom = datenum(epoch(start_time));
-        timeto = datenum(epoch(stop_time));
+            timefrom = datenum(epoch(start_time));
+            timeto = datenum(epoch(stop_time));
 
-        q = 1.602177335e-19;
-        aem = 1.66054021010e-24;
+            q = 1.602177335e-19;
+            aem = 1.66054021010e-24;
 
-        choose_ind = find(epoch>=timefrom & epoch<=timeto);
-        epoch = epoch(choose_ind);
-        %assignin('base', 'epch', epoch)
-        %assignin('base', 'ch_ind', choose_ind)
-        assignin('base', 'eflx', eflux)
-        eflux = eflux(:, :, :, choose_ind);
-        swp_ind = swp_ind(choose_ind);
+            choose_ind = find(epoch>=timefrom & epoch<=timeto);
+            epoch = epoch(choose_ind);
+            %assignin('base', 'epch', epoch)
+            %assignin('base', 'ch_ind', choose_ind)
+            assignin('base', 'eflx', eflux)
+            eflux = eflux(:, :, :, choose_ind);
+            swp_ind = swp_ind(choose_ind);
 
-        d_mass = 3; %a.u. (accuracy)
-        swpind = swp_ind(1);
-        m = mass_arr(16, swpind+1, 1, :);
-        mass_num_range = find(( m>(mass-d_mass) )&( m<(mass+d_mass) ));
-        mass_num = round((mass_num_range(1)+mass_num_range(end))/2);
+            d_mass = 3; %a.u. (accuracy)
+            swpind = swp_ind(1);
+            m = mass_arr(16, swpind+1, 1, :);
+            mass_num_range = find(( m>(mass-d_mass) )&( m<(mass+d_mass) ));
+            mass_num = round((mass_num_range(1)+mass_num_range(end))/2);
 
-        onemass_eflux = reshape( sum(eflux(:, :, mass_num_range, :), 3), [size(eflux, 1) size(eflux, 2) size(eflux, 4)] );
-        v = 1*sqrt(2*q*energy./(aem*mass_arr));
+            onemass_eflux = reshape( sum(eflux(:, :, mass_num_range, :), 3), [size(eflux, 1) size(eflux, 2) size(eflux, 4)] );
+            v = 1*sqrt(2*q*energy./(aem*mass_arr));
 
-        phsdensity = zeros(size(onemass_eflux));
-        for i = 1:size(onemass_eflux, 3)
-            m_sq = aem*permute(squeeze(mass_arr(:, swp_ind(i)+1, :, mass_num)), [2 1]);
-            e_sq = permute(squeeze(energy(:, swp_ind(i)+1, :, mass_num)), [2 1]);
-            phsdensity(:, :, i) = 1e4*0.5*onemass_eflux(:, :, i).*(m_sq./(e_sq*q)).^2;
-        end
+            phsdensity = zeros(size(onemass_eflux));
+            for i = 1:size(onemass_eflux, 3)
+                m_sq = aem*permute(squeeze(mass_arr(:, swp_ind(i)+1, :, mass_num)), [2 1]);
+                e_sq = permute(squeeze(energy(:, swp_ind(i)+1, :, mass_num)), [2 1]);
+                phsdensity(:, :, i) = 1e4*0.5*onemass_eflux(:, :, i).*(m_sq./(e_sq*q)).^2;
+            end
 
-        concentration = zeros(length(epoch), 1);
-        for timenum = 1:length(epoch)
-            for en = 1:nenergy
-                for nphi = 1:nanode
-                    for ntheta = 1:ndef
-                            bin = ndef*(nphi-1)+ntheta;
-                            swp_i = swp_ind(timenum);
-                            v_ccl = v(en,swp_i+1,bin,mass_num); 
-                            phdens = phsdensity(bin, en, timenum); 
+            concentration = zeros(length(epoch), 1);
+            for timenum = 1:length(epoch)
+                for en = 1:nenergy
+                    for nphi = 1:nanode
+                        for ntheta = 1:ndef
+                                bin = ndef*(nphi-1)+ntheta;
+                                swp_i = swp_ind(timenum);
+                                v_ccl = v(en,swp_i+1,bin,mass_num); 
+                                phdens = phsdensity(bin, en, timenum); 
 
-                            volume = q*v_ccl*domega(en,swp_i+1,bin,mass_num)*denergy(en,swp_i+1,bin,mass_num)/(aem*mass_arr(en,swp_i+1,bin,mass_num));
-                            concentration(timenum) = concentration(timenum) + volume*phdens;                  
+                                volume = q*v_ccl*domega(en,swp_i+1,bin,mass_num)*denergy(en,swp_i+1,bin,mass_num)/(aem*mass_arr(en,swp_i+1,bin,mass_num));
+                                concentration(timenum) = concentration(timenum) + volume*phdens;                  
+                        end
                     end
                 end
             end
+
+            axes(ax);
+            if (log==1)
+                semilogy(epoch, concentration/1e6, 'linewidth', 0.5)
+            else
+                plot(epoch,concentration/1e6, 'linewidth', 0.5)
+            end
+
+            ylabel('n, cm^{-3}')
+            datetick('x','HH:MM:SS');
+            set (ax, 'fontsize', 8);
+            grid on
+
+            %xlim
+            averind = round(size(epoch, 1)/2);
+            day = datestr(epoch(averind), 'dd-mmm-yyyy');
+            t1 = [day, ' ', start_str]; t2 = [day, ' ', stop_str];
+            xlim([datenum(t1), datenum(t2)])
         end
-
-        axes(ax);
-        if (log==1)
-            semilogy(epoch, concentration/1e6, 'linewidth', 0.5)
-        else
-            plot(epoch,concentration/1e6, 'linewidth', 0.5)
+        
+    elseif strcmp(file_type, 'sql')
+        
+        col_name = '';
+        vars = 0;
+        
+        if mass == 1
+            col_name = 'ts_concentration_H';
+            ylab = 'n_H, cm^{-3}';
+        elseif mass == 16
+            col_name = 'ts_concentration_O';
+            ylab = 'n_O, cm^{-3}';
+        elseif mass == 32
+            col_name = 'ts_concentration_O2';
+            ylab = 'n_{O^2}, cm^{-3}';    
         end
-
-        ylabel('n, cm^{-3}')
-        datetick('x','HH:MM:SS');
-        set (ax, 'fontsize', 8);
-        grid on
-
-        %xlim
-        averind = round(size(epoch, 1)/2);
-        day = datestr(epoch(averind), 'dd-mmm-yyyy');
-        t1 = [day, ' ', start_str]; t2 = [day, ' ', stop_str];
-        xlim([datenum(t1), datenum(t2)])
-    end
+        
+        if ~strcmp(col_name, '')            
+            cd('../Aux_Fncs')
+            vars = feval('read_from_sql', '', '', 'maven.test', {col_name}, date, start_time, stop_time);  
+            cd('../Scripts')
+        end
+        
+        if ~isnumeric(vars)
+            vars = cell2mat(vars); % from cell to double
+            data_to_plot = [vars(:,1), vars(:,2)]; % get two-column data to plot
+        
+            cd('../Aux_Fncs')
+            feval('plot_from_sql', ax, data_to_plot, ylab, date, start_time, stop_time);
+            cd('../Scripts')
+        end
+        
+        message = '';%['"', files_final, '"', ' was successfully uploaded and the system is ready to plot ', chosenfunc];
+        error = '0_no_error';
+        
+    end    
     
 function [file, msg, err] = get_file(date, filefolderpath)
     
@@ -98,4 +137,5 @@ function [file, msg, err] = get_file(date, filefolderpath)
     
     cd('../Aux_Fncs')
     [file, msg, err] = feval('GetFile', chosenfunc, filetype, extensions, date, filefolderpath); 
-    cd('../Scripts')        
+    cd('../Scripts')    
+    
